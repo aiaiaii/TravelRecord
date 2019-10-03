e1c84d956d0fa4f156f74e895bf3eb4771396c92
kalastus33 <kalastus33@yahoo.co.jp>
2019-10-03 16:48:48 +0900

    changed some codes

--- signup.php
+++ signup.php
@@ -1,115 +1,7 @@
 <?php
-//ログを取るかどうか
-ini_set('log_errors','on');
-//ログを出力ファイルを指定
-ini_set('error_log','php.log');
 
-//エラーメッセージを定数に設定
-define('MSG01','入力必須です');
-define('MSG02','Emailの形式で入力してください');
-define('MSG03','パスワード（再入力）があっていません');
-define('MSG04','半角英数字のみご利用いただけます');
-define('MSG05','6文字以上で入力してください');
-define('MSG06','255文字以内で入力してください');
-define('MSG07','エラーが発生しました。しばらく経ってからやり直してください');
-define('MSG08','そのEmailは既に登録されています');
-define('MSG09','3文字以上で入力してください')
-
-//エラーメッセージ格納用の配列
-$err_msg = array();
-
-//バリデーション関数（未入力チェック）
-function validRequired($str,$key){
-  if(empty($str)){
-    global $err_msg;
-    $err_msg[$key] = MSG01;
-  }
-}
-//バリデーション関数（username文字数チェック）
-function validMinLenUsername($str,$key,$min = 3){
-  if(mb_strlen($str) < $min){
-    global $err_msg;
-    $err_msg[$key] = MSG09;
-  }
-}
-//バリデーション関数（Email形式チェック）
-function validEmail($str,$key){
-  if(!pregmatch("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/",$str)){
-    global $err_msg;
-    $err_msg[$key] = MSG02;
-  }
-}
-//バリデーション関数(Email重複チェック)
-function validEmailDup($email){
-  global $err_msg;
-  //例外処理
-  try {
-    // DBへ接続
-    $dbh = dbConnect();
-    // SQL文を作成
-    $sql = 'SELECT count(*) FROM users WHERE email = :email';
-    $data = array(':email' => $email);
-    //クエリ実行
-    $stmt = queryPost($dbh,$sql,$data);
-    // クエリ結果の値を取得
-    $result = $stmt->fetch(PDO::FETCH_ASSOC)
-    return $stmt; // なぜ$resultではなく、$stmtを返すのか？
-  }
-}
-//バリデーション関数（同値チェック）
-function validMatch($str1, $str2, $key){
-  if($str1 !== $str2){
-    global $err_msg;
-    $err_msg[$key] = MSG03;
-  }
-}
-//バリデーション関数（最小文字数チェック）
-function validMinLen($str, $key, $min = 6){
-  if(mb_strlen($str) < $min){
-    global $err_msg;
-    $err_msg[$key] = MSG05;
-  }
-}
-//バリデーション関数（最大文字数チェック）
-function validMaxLen($str, $key, $max = 255){
-  if(mb_strlen($str) > $max){
-    global $err_msg;
-    $err_msg[$key] = MSG06;
-  }
-}
-//バリデーション関数（半角チェック）
-function validHalf($str, $key){
-  if(!preg_match("/^[a-zA-Z0-9]+$/",$key)){
-    global $err_msg;
-    $err_msg[$key] = MSG04;
-  }
-}
-//DB接続関数
-function dbConnect(){
-  $dsn = 'mysql:dbname=travelrecord;host=localhost;charset=utf8';
-  $user = 'root'
-  $password = 'root'
-  $options = array(
-  // SQL実行失敗時にはエラーコードのみ設定
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
-    // デフォルトフェッチモードを連想配列形式に設定
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    // バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
-    // SELECTで得た結果に対してもrowCountメソッドを使えるようにする
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
-  );
-  //PDOオブジェクト生成（DBへ接続）
-  $dbh = new PDO($dsn, $user, $password, $options);
-  return $dbh;
-}
-//SQL実行関数
-function queryPost($dbh,$sql,$data){
-  //クエリー作成
-  $stmt = $dbh->prepare($sql);
-  //プレースホルダに値をセットし、SQL文を実行
-  $stmt->execute($data);
-  return $stmt;
-}
+//共通変数・関数ファイルを読込み
+require('function.php');
 
 //POST送信されているか
 if(!empty($_POST)){
@@ -129,19 +21,20 @@ if(!empty($_POST)){
   if(empty($err_msg)){
     //usernameの文字数チェック
     validMinLenUsername($username,'username');
+    
     //Emailの形式チェック
     validEmail($email,'email');
     //emailの最大文字数チェック
-    validMaxLen($emial,'email');
+    validMaxLen($email,'email');
     //emailの最小文字数チェック
-    validMinLen($email,'email')
+    validMinLen($email,'email');
     
     //パスワード半角英数字チェック
-    validHalf($pass,'pass');
+    validHalf($pass, 'pass');
     //パスワード最大文字数チェック
     validMaxLen($pass,'pass');
-    //パスワード最小文字数チェック
-    validMinLen($pass,'pass');
+    //email重複チェック
+    validEmailDup($email);
     
     if(empty($err_msg)){
       
@@ -155,8 +48,8 @@ if(!empty($_POST)){
       //DB接続
       $dbh = dbConnect();
       //SQL文作成
-      $sql = 'INSERT INTO users (username,email,password,login_time,create_date) VALUES(:email,:pass,:login_time,:create_date)';
-      $data = array(':username' => $username, ':email' => $email, ':pass' => password_hash($pass,PASSWORD_DEFAULT), ':logintime' =>datedate('Y-m-d H:i:s'),':create_date' => date('Y-m-d H:i:s'));
+      $sql = 'INSERT INTO users(username,email,password,login_time,create_date) VALUES(:username,:email,:pass,:login_time,:create_date)';
+      $data = array(':username' => $username, ':email' => $email, ':pass' => password_hash($pass,PASSWORD_DEFAULT),':login_time' => date('Y-m-d H:i:s'), 'create_date' => date('Y-m-d H:i:s'));
       //クエリ実行
       queryPost($dbh,$sql,$data);
       
@@ -177,26 +70,28 @@ if(!empty($_POST)){
 <head>
   <meta charset="utf-8">
   <title>ユーザー登録 | travel record</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/reset.css">
   <link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Text:400,700&display=swap" rel="stylesheet">
-  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:400,700&display=swap" rel="stylesheet">
+</head>
 
-<body class="page-signup page-1colum">
+<body class="page-signup page-1colum" background="img/luca-bravo-UKJev6G45f0-unsplash.jpg">
   <!-- メニュー -->
-  <header>
-    <div class="site-width">
+  <header class="header">
+    <div class="site-width clearfix">
       <h1><a href="index.php">Travel Record</a></h1>
       <nav id="top-nav">
         <ul>
+         <li><a href="login.php" class="btn">ログイン</a></li>
           <li><a href="signup.php" class="btn btn-primary">ユーザー登録</a></li>
-          <li><a href="login.php">ログイン</a></li>
         </ul>
       </nav>
     </div>
   </header>
 
   <!-- メインコンテンツ -->
-  <div id="contents" class="site-width">
+  <div id="contents" class="site-width page-1column">
 
     <!-- Main -->
     <section id="main">
@@ -205,29 +100,29 @@ if(!empty($_POST)){
         <form action="" method="post" class="form">
           <h2 class="title">ユーザー登録</h2>
           <div class="area-msg">
-          <?php
+            <?php
             if(!empty($err_msg['common'])) echo $err_msg['common'];
             ?>
           </div>
-          <label class="">
-            ユーザー名
-            <input type="text" name="userneame" value="<?php if(!empty($_POST['username'])) echo $_POST['username']; ?>">
+          <label class="<?php if(!empty($err_msg['username'])) echo 'err'; ?>">
+            ユーザー名<br>
+            <input type="text" name="username" value="<?php if(!empty($_POST['username'])) echo $_POST['username']; ?>">
           </label>
           <div class="area-msg">
             <?php
             if(!empty($err_msg['username'])) echo $err_msg['username'];
             ?>
           </div>
-          <label class="">
-            Email
+          <label class="<?php if(!empty($err_msg['email'])) echo 'err'; ?>">
+            Email<br>
             <input type="text" name="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email']; ?>">
           </label>
           <div class="area-msg">
             <?php 
             if(!empty($err_msg['email'])) echo $err_msg['email']; ?>
           </div>
-          <label class="<?php (!empty($err_msg['email'])) echo 'err'; ?>">
-            パスワード<span>※英数字6文字以上</span>
+          <label class="<?php if(!empty($err_msg['pass'])) echo 'err'; ?>">
+            パスワード<span>※英数字6文字以上</span><br>
             <input type="password" name="pass" value="<?php if(!empty($_POST['pass'])) echo $_POST['pass'] ;?>">
           </label>
           <div class="area-msg">
@@ -236,8 +131,8 @@ if(!empty($_POST)){
             ?>
           </div>
           <label class="<?php if(!empty($err_msg['pass_re'])) echo 'err'; ?>">
-            パスワード（再入力）
-            <input type="password" name="pass_re" value="<?php (!empty($_POST['pass_re'])) echo $_POST['pass_re']; ?>">
+            パスワード（再入力）<br>
+            <input type="password" name="pass_re" value="<?php if(!empty($_POST['pass_re'])) echo $_POST['pass_re']; ?>">
           </label>
           <div class="area-msg">
             <?php
@@ -251,12 +146,12 @@ if(!empty($_POST)){
       </div>
     </section>
   </div>
-  
+
   <!-- footer -->
-  <footer id="footer">
-    Copyright <a href="">Record Travel</a>. All RIghts Reserved.
+  <footer id="footer" class="site-width">
+    Copyright <a href="">Travel Record</a>. All RIghts Reserved.
   </footer>
-  
+
   <script src="js/vendor/jquery-2.2.2.min.js"></script>
 </body>
 </head>

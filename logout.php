fb4c7980cd5ad33cc75a1961a165dca5d0323c19
kalastus33 <kalastus33@yahoo.co.jp>
2019-10-06 18:52:11 +0900

    first commint

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「　ログアウトページ　');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+session_destroy();
+header("Location:login.php");
+?>

0c095cb302c97c57ef3e33f0d03423b5dade3731
kalastus33 <kalastus33@yahoo.co.jp>
2019-10-06 18:53:05 +0900

    first commit

---
+++ mypage.php
@@ -0,0 +1,21 @@
+<?php
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「　マイページ　');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+//ログイン認証
+require('auth.php');
+?>
+<?php
+$siteTitle = 'マイページ';
+require('head.php');
+?>
+<?php
+require('header.php');
+?>
+
+ <img src="img/飛行機の画像.jpg" alt="" style="width:100%;">
+  
